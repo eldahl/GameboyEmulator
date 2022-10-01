60cbda7f00e3f0c175fda27f8a526ae3d21f81d3
Ebberx <esben_dahl@hotmail.com>
2022-10-01 22:59:54 +0200

    a couple instructions, corrected DMG binary

--- GBE/gbe.cpp
+++ GBE/gbe.cpp
@@ -11,7 +11,20 @@ int main(int argc, char* argv[]) {
 	cart* _cart = new cart("../tetris.gb");
 
 	cpu* CPU = new cpu(_cart->getRom(), _cart->getRomSize());
+
+	///////////////////
+
+	uint8_t reg_A	= 0b00100000;
+	uint8_t d8		= 0000000011;
+
+	//if (((reg_A) & 0xF) == 0x0)
+	if (((0b00000) & 0xF) == 0x0)
+		std::cout << "Borrow bit 4";
+
+	while (true);
 	
+	///////////////////
+
 	while (running) {
 		CPU->cycle();
 
