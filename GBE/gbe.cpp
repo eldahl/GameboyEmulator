4ec0bb10d915a4c06292dab1cb9d22b506a2faa3
Esben Christensen <71466904+blexaer@users.noreply.github.com>
2022-12-28 16:35:15 +0100

    Added logging of CPU instructions

--- GBE/gbe.cpp
+++ GBE/gbe.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "cpu.h"
 #include "cart.h"
 #include "display.h"
@@ -7,6 +8,10 @@
 bool running = true;
 
 int main(int argc, char* argv[]) {
+
+	std::ofstream out("out.txt");
+	std::streambuf* coutbuf = std::cout.rdbuf(); //save old buf
+	std::cout.rdbuf(out.rdbuf()); //redirect std::cout to out.txt!
 
 	// Load cartridge
 	cart* _cart = new cart("../roms/tetris.gb");
